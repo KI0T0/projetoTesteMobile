231c0256a2a3bc5e88e935e40a48d2285c44f224
Felipe Rodrigo de Souza Gonçalves <f_g@outlook.com.br>
2020-11-10 20:24:36 -0300

    Atribuição de ids na tela de cadastro e aprendendo como utilizar strings.xml.

--- app/src/main/java/com/example/projetoteste/ActCadCliente.java
+++ app/src/main/java/com/example/projetoteste/ActCadCliente.java
@@ -1,16 +1,17 @@
 package com.example.projetoteste;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
+import android.widget.EditText;
 
 public class ActCadCliente extends AppCompatActivity {
+
+    private EditText edtNome;
+    private EditText edtEndereco;
+    private EditText edtEmail;
+    private EditText edtTelefone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +19,11 @@ public class ActCadCliente extends AppCompatActivity {
         setContentView(R.layout.act_cad_cliente);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        edtNome = (EditText)findViewById(R.id.edtNome);
+        edtEndereco = (EditText)findViewById(R.id.edtEndereco);
+        edtEmail = (EditText)findViewById(R.id.edtEmail);
+        edtTelefone = (EditText)findViewById(R.id.edtTelefone);
+
     }
 }
